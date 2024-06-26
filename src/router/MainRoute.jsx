a0831603bb429509990cb327891b2c3ec519770b
Anisur Rahman <anis.dapunia@gmail.com>
2024-06-26 22:26:57 +0600

    secure and public custom hook added

--- src/router/MainRoute.jsx
+++ src/router/MainRoute.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import SignUp from "../pages/loginRegister/SignUp";
-import Login from "../components/Login/Login";
 import Home from "../pages/home/Home";
+import Login from "../pages/loginRegister/Login/Login";
 
 export const router = createBrowserRouter([
   {
